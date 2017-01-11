2b5e2f55edf4f3d1f30de7ad2f1c9fcd8e301bdc
Antoine FRANCE <afrance@clever-age.com>
2017-01-11 16:52:03 +0100

    :arrow_up: Update php view :)

--- Resources/views/Form/ewz_recaptcha_widget.html.php
+++ Resources/views/Form/ewz_recaptcha_widget.html.php
@@ -1,6 +1,20 @@
 <?php if ($ewz_recaptcha_enabled): ?>
     <?php if (!$ewz_recaptcha_ajax): ?>
-        <script src="<?php echo $url_challenge ?>" type="text/javascript"></script>
+        <?php if ($load_api): ?>
+        <script type="text/javascript" src="<?php echo $url_challenge ?>&amp;onload=captchaCallback&amp;render=explicit" <?php echo $attr['options']['defer'] ?> <?php echo $attr['options']['async'] ?>></script>
+        <?php endif; ?>
+        <script type="text/javascript">
+            var captchaCallback = function() {
+                var captchas = document.getElementsByClassName('g-recaptcha');
+
+                for (var i = 0; i < captchas.length; i++) {
+                    grecaptcha.render(captchas[i], {
+                        'sitekey': '<?php echo $public_key ?>',
+                        'theme': '<?php echo $attr['options']['theme'] ?>'
+                    });
+                }
+            };
+        </script>
         <div class="g-recaptcha" data-theme="<?php echo $attr['options']['theme'] ?>" data-size="<?php echo $attr['options']['size'] ?>" data-type="<?php echo $attr['options']['type'] ?>" data-sitekey="<?php echo $public_key ?>" <?php if (isset($attr['options']['callback'])): ?>data-callback="<?php echo $attr['options']['callback'] ?>"<?php endif ?> <?php if (isset($attr['options']['expiredCallback'])): ?>data-expired-callback="<?php echo $attr['options']['expiredCallback'] ?>"<?php endif ?>></div>
         <noscript>
             <div style="width: 302px; height: 352px;">
@@ -13,11 +27,11 @@
                         </iframe>
                     </div>
                     <div style="width: 250px; height: 80px; position: absolute; border-style: none; bottom: 21px; left: 25px; margin: 0; padding: 0; right: 25px;">
-                        <textarea id="g-recaptcha-response" name="g-recaptcha-response"
-                                  class="g-recaptcha-response"
-                                  style="width: 250px; height: 80px; border: 1px solid #c1c1c1; margin: 0; padding: 0; resize: none;"
-                        >
-                        </textarea>
+                            <textarea id="g-recaptcha-response" name="g-recaptcha-response"
+                                      class="g-recaptcha-response"
+                                      style="width: 250px; height: 80px; border: 1px solid #c1c1c1; margin: 0; padding: 0; resize: none;"
+                            >
+                            </textarea>
                     </div>
                 </div>
             </div>
@@ -26,13 +40,17 @@
         <div id="ewz_recaptcha_div"></div>
 
         <script type="text/javascript">
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.onload = function() {
-                Recaptcha.create('<?php echo $public_key ?>', 'ewz_recaptcha_div', <?php echo json_encode($attr['options']) ?>);
-            };
-            script.src = '<?php echo $url_api ?>';
-            document.getElementsByTagName('head')[0].appendChild(script);
+            (function() {
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.onload = function() {
+                    Recaptcha.create('<?php echo $public_key ?>', 'ewz_recaptcha_div', <?php echo json_encode($attr['options']) ?>);
+                };
+                script.src = '<?php echo $url_api ?>';
+                <?php if ($attr['options']['defer']): ?>script.defer = true;<? endif; ?>
+                <?php if ($attr['options']['async']): ?>script.async = true;<? endif; ?>
+                document.getElementsByTagName('head')[0].appendChild(script);
+            })();
         </script>
     <?php endif ?>
 <?php endif ?>
